1f417f06afe0b69f12c5b41d56637e72efe9f833
Logan Bellemare <ellabellemare@gmail.com>
2023-11-14 20:01:57 -0500

    finished lesson solving problems with Sets, Maps, and arrays

--- src/intersection/index.js
+++ src/intersection/index.js
@@ -1,3 +1,14 @@
-function intersection(a, b) {}
+function intersection(a, b) {
+  const mapA = new Map();
+  const result = new Set();
+  a.forEach((element) => mapA.set(element, element));
+  b.forEach((element) => {
+    const foundElement = mapA.get(element);
+    if (foundElement) {
+      result.add(element);
+    }
+  });
+  return [...result];
+}
 
 module.exports = intersection;
--- src/sumPairs/index.js
+++ src/sumPairs/index.js
@@ -3,6 +3,20 @@
  * @param {Array} A an array of numbers
  * @param {Number} N an integer value
  */
-function sumPairs(A, N) {}
+function sumPairs(A, N) {
+  const numbers = new Set();
+  const solution = new Map();
+  A.forEach((element) => numbers.add(element));
+  A.forEach((element) => {
+    const diff = N - element;
+    if (diff !== element) {
+      const foundNumber = numbers.has(diff);
+      if (foundNumber) {
+        solution.set(Math.min(element, diff), Math.max(element, diff));
+      }
+    }
+  });
+  return [...solution];
+}
 
 module.exports = sumPairs;
